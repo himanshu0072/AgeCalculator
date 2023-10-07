598c6a731ed9f6dc05b5a34d3a794dbe0f413d15
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-10-07 23:57:28 +0530

    Getting inputs from users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [dateOfBirth, setDateOfBirth] = useState("");
+
+  const handleDateChange = (event) => {
+    setDateOfBirth(event.target.value || "");
+    console.log(dateOfBirth);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Users date of birth is:", dateOfBirth);
+    // Add your logic to handle the form submission with the selected dateOfBirth here
+  };
+
   return (
     <div className="Container">
-      <form className="Form">
+      <form className="Form" onSubmit={handleSubmit}>
         <label htmlFor="dob">Select your birth date</label>
 
-        <input type="date" name="dob" id="dob" className="dob"></input>
         <input
-          type="button"
-          name="Submit"
-          className="submit"
-          value={"→"}
+          type="date"
+          name="dob"
+          id="dob"
+          className="dob"
+          value={dateOfBirth || ""}
+          onChange={handleDateChange} // Use onChange for capturing date changes
         ></input>
+        <button type="submit" name="Submit" className="submit">
+          →
+        </button>
       </form>
     </div>
   );
